7b024456d103c382ff907fa894d8839237897e79
sergeisitsko <xssitsko@chamberlain.com>
2021-02-14 13:41:27 +0300

    di

---
+++ app/src/main/java/com/quiz/users/di/Modules.kt
@@ -0,0 +1,52 @@
+package com.quiz.users.di
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.quiz.users.api.ApiInterface
+import com.quiz.users.ui.UserListViewModel
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+val viewModels = module {
+    viewModel { UserListViewModel(get()) }
+}
+
+val apiModule = module {
+    single {
+
+        val tokenInterceptor = Interceptor { chain ->
+            val request =
+                chain
+                    .request()
+                    .newBuilder()
+                    .addHeader("Authorization", "c3c2f4dd13817c990f678478843229a2e21f5649b236b37e75d308c035685600")
+                    .build()
+
+            chain.proceed(request)
+        }
+
+        val logInterceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.HEADERS
+        }
+
+        val okHttpClient =
+            OkHttpClient.Builder()
+                .addInterceptor(tokenInterceptor)
+                .addInterceptor(logInterceptor)
+                .build()
+
+        val retrofit =
+            Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl("https://gorest.co.in/public-api/")
+                .addConverterFactory(MoshiConverterFactory.create())
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .build()
+
+        retrofit.create(ApiInterface::class.java)
+    }
+}
